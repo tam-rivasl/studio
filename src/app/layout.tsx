c48317c6ebfa8bb83a434914a64fb7f63dacf50f
tamara rivas <tam.trivas@gmail.com>
2025-07-12 00:12:10 +0000

    haz que el modo dark sea por defecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css"; // Estilos globales de la aplicación.
 import { ThemeProvider } from "@/components/providers/theme-provider"; // Proveedor de temas (claro/oscuro).
 import { Toaster } from "@/components/ui/toaster"; // Componente para mostrar notificaciones (toasts).
+import { cn } from "@/lib/utils";
 
 // Metadatos de la página, importantes para SEO y para cómo se muestra en el navegador.
 export const metadata: Metadata = {
@@ -24,13 +25,21 @@ export default function RootLayout({
   return (
     // Se define el HTML con el idioma y se suprime una advertencia de hidratación.
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
+      </head>
       {/* El cuerpo de la página con una clase para antialiasing para un texto más suave. */}
-      <body className="antialiased">
+      <body
+        className={cn(
+          "antialiased font-body bg-background"
+        )}
+      >
         {/* ThemeProvider envuelve a los hijos para darles acceso al contexto del tema. */}
         <ThemeProvider
           attribute="class" // El tema se aplicará como una clase en el elemento HTML.
-          defaultTheme="system" // Tema por defecto según el sistema operativo.
+          defaultTheme="dark" // Tema por defecto será oscuro.
           enableSystem // Permite cambiar el tema según la configuración del sistema.
           disableTransitionOnChange // Deshabilita la transición al cambiar de tema para evitar parpadeos.
         >
